25353f9c9502def8a16b8cf54f6a7137eb8d2e06
Bruno Ferreira <brunoferreira_@outlook.com>
2021-08-09 01:15:11 -0300

    Faz newsletter

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../molecules/Header";
+import { Newsletter } from "../../molecules/Newsletter";
 import { Showcase } from "../../molecules/Showcase";
 
 export function Home() {
@@ -6,6 +7,7 @@ export function Home() {
     <div className="page-home">
       <Header />
       <Showcase title="Mais vendidos" />
+      <Newsletter />
     </div>
   );
 }
